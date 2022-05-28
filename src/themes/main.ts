e2763b49e181ca4a23ba6c6dc682105ca52a1ca5
Benjamin Modayil <modayilme@gmail.com>
2022-05-27 22:54:38 -0400

    fix default colors

--- src/themes/main.ts
+++ src/themes/main.ts
@@ -1,7 +1,3 @@
-/* eslint-disable import/no-webpack-loader-syntax */
-import { darken, lighten } from 'polished';
-
-// These colors are taken from source/scss/utilities/_colors.scss
 const colorsFromUtilities = {
   white: '#ffffff',
   black: '#001037',
@@ -11,22 +7,6 @@ const mainTheme = () => {
   return {
     colors: {
       ...colorsFromUtilities,
-
-      // Below are "official" defined colors coming from new designs
-      // Eventually, these will be used in place of the sass variables in _colors
-      hasPhi: '#98E6D5',
-      hasProviderError: '#FF99FC',
-      activeGreen: '#42AE87',
-      hotPink: '#D10373',
-      hasProfanity: colorsFromUtilities.yellow,
-      sentiment: {
-        noSentiment: colorsFromUtilities.disabledGray,
-        veryPositive: '#52C99A',
-        positive: '#ABC96E',
-        neutral: '#F2C14D',
-        negative: '#EF9052',
-        veryNegative: '#EB5E56',
-      },
     },
     fontSizes: {
       12: `${12 / 16}rem`,
